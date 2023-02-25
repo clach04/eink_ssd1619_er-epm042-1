34503b3b07f67dcac307573cd34d8130acff2dae
clach04 <clach04@gmail.com>
2023-02-25 14:28:23 -0800

    refactor into class

--- ER-EPM042-1_Raspberry_Pi/ER-EPM042A1-1R/python/epd.py
+++ ER-EPM042-1_Raspberry_Pi/ER-EPM042A1-1R/python/epd.py
@@ -62,156 +62,159 @@ BORDER_WAVEFORM_CONTROL = 0x3C
 DISPLAY_UPDATE_CONTROL_1 = 0x21
 
 
-
-def digital_write(pin, value):
-    # needs GPIO - want something portable
-    gpio.output(pin, value)
-
-def digital_read(pin):
-    return gpio.input(pin)
-
-def spi_writebyte(data):
-    spi.writebytes([data])
-
-def epd_send_command(command):
-    digital_write(DC_PIN, 0)
-    digital_write(CS_PIN, 0)
-    spi_writebyte(command)
-    digital_write(CS_PIN, 1)
-
-def epd_send_data(data):
-    digital_write(DC_PIN, 1)
-    digital_write(CS_PIN, 0)
-    spi_writebyte(data)
-    digital_write(CS_PIN, 1)
-
 def delay_ms(delaytime):
     time.sleep(delaytime / 1000.0)
 
-def epd_wait_until_idle():
-    while(digital_read(BUSY_PIN) == 1):      #  0/LOW: idle, 1/HIGH: busy
-        delay_ms(100)
 
-def epd_turn_on_display():
-    epd_send_command(DISPLAY_UPDATE_CONTROL_2)
-    epd_send_data(0xC7)
-    epd_send_command(MASTER_ACTIVATION)
-    epd_wait_until_idle()
-    
-def epd_set_windows(x_start, y_start, x_end, y_end):
-    epd_send_command(SET_RAM_X_ADDRESS_START_END_POSITION)
-    epd_send_data((x_start >> 3) & 0xFF)
-    epd_send_data((x_end >> 3) & 0xFF)
+class Epd:
+    def __init__(self, bus=0, device=0)
+        # bus, device = 0, 0  # /dev/spidev<bus>.<device>
+        self.spi = spidev.SpiDev()
+        self.spi.open(bus, device)
 
-    epd_send_command(SET_RAM_Y_ADDRESS_START_END_POSITION)
-    epd_send_data(y_start & 0xFF)
-    epd_send_data((y_start >> 8) & 0xFF)
-    epd_send_data(y_end & 0xFF)
-    epd_send_data((y_end >> 8) & 0xFF)
+        # init GPIO
+        gpio.setmode(gpio.BCM)
+        gpio.setwarnings(False)
+        gpio.setup(RST_PIN, gpio.OUT)
+        gpio.setup(DC_PIN, gpio.OUT)
+        gpio.setup(CS_PIN, gpio.OUT)
+        gpio.setup(BUSY_PIN, gpio.IN)
 
-def epd_set_cursor(x_start, y_start):
-    epd_send_command(SET_RAM_X_ADDRESS_COUNTER)
-    epd_send_data((x_start >> 3) & 0xFF)
+        self.spi.max_speed_hz = 32000000  # is this 32Mhz? units are not documented in wiringpim struct implies this is hz
+        self.spi.mode = 0b00
 
-    epd_send_command(SET_RAM_Y_ADDRESS_COUNTER)
-    epd_send_data(y_start & 0xFF)
-    epd_send_data((y_start >> 8) & 0xFF)
+    def digital_write(self, pin, value):
+        # needs GPIO - want something portable
+        gpio.output(pin, value)
 
-def epd_clear():
-    #width = (EPD_WIDTH % 8 == 0)? (EPD_WIDTH / 8 ): (EPD_WIDTH / 8 + 1);
-    width = (EPD_WIDTH / 8 ) if (EPD_WIDTH % 8 == 0) else (EPD_WIDTH / 8 + 1)
-    width = int(width)
-    height = EPD_HEIGHT;
-    epd_set_windows(0, 0, EPD_WIDTH, EPD_HEIGHT)
-    # for loop black
-    for j in range(height):
-        epd_set_cursor(0, j)
-        epd_send_command(WRITE_RAM)
-        for i in range(width):
-            epd_send_data(0XFF)
+    def digital_read(self, pin):
+        return gpio.input(pin)
 
-    # for loop red
-    for j in range(height):
-        epd_set_cursor(0, j)
-        epd_send_command(WRITE_RAM_RED)
-        for i in range(width):
-            epd_send_data(0XFF)
-    epd_turn_on_display()
+    def spi_writebyte(self, data):
+        self.spi.writebytes([data])
 
-def epd_sleep():
-    epd_send_command(DEEP_SLEEP_MODE)
-    epd_send_data(0x01)
+    def epd_send_command(self, command):
+        self.digital_write(DC_PIN, 0)
+        self.digital_write(CS_PIN, 0)
+        self.spi_writebyte(command)
+        self.digital_write(CS_PIN, 1)
 
-def epd_reset():
-    digital_write(RST_PIN, 1)
-    delay_ms(200)
-    digital_write(RST_PIN, 0)
-    delay_ms(200)
-    digital_write(RST_PIN, 1)
-    delay_ms(200)
+    def epd_send_data(self, data):
+        self.digital_write(DC_PIN, 1)
+        self.digital_write(CS_PIN, 0)
+        self.spi_writebyte(data)
+        self.digital_write(CS_PIN, 1)
 
-def epd_init():
-    epd_reset()
+    def epd_wait_until_idle(self):
+        while(self.digital_read(BUSY_PIN) == 1):      #  0/LOW: idle, 1/HIGH: busy
+            delay_ms(100)
 
-    epd_send_command(0x74)
-    epd_send_data(0x54)
-    epd_send_command(0x7E)
-    epd_send_data(0x3B)
-    epd_send_command(0x2B)   #  Reduce glitch under ACVCOM  
-    epd_send_data(0x04)           
-    epd_send_data(0x63)
+    def epd_turn_on_display(self):
+        self.epd_send_command(DISPLAY_UPDATE_CONTROL_2)
+        self.epd_send_data(0xC7)
+        self.epd_send_command(MASTER_ACTIVATION)
+        self.epd_wait_until_idle()
         
-    epd_send_command(0x0C)   #  Soft start setting
-    epd_send_data(0x8E)           
-    epd_send_data(0x8C)
-    epd_send_data(0x85)
-    epd_send_data(0x3F)    
+    def epd_set_windows(self, x_start, y_start, x_end, y_end):
+        self.epd_send_command(SET_RAM_X_ADDRESS_START_END_POSITION)
+        self.epd_send_data((x_start >> 3) & 0xFF)
+        self.epd_send_data((x_end >> 3) & 0xFF)
 
+        self.epd_send_command(SET_RAM_Y_ADDRESS_START_END_POSITION)
+        self.epd_send_data(y_start & 0xFF)
+        self.epd_send_data((y_start >> 8) & 0xFF)
+        self.epd_send_data(y_end & 0xFF)
+        self.epd_send_data((y_end >> 8) & 0xFF)
 
-    epd_send_command(DRIVER_OUTPUT_CONTROL)   #  Driver Output control  Set MUX as 300
-    epd_send_data(0x2B)           
-    epd_send_data(0x01)
-    epd_send_data(0x00)  
-    epd_send_command(DATA_ENTRY_MODE_SETTING)   #  Data Entry mode setting
-    epd_send_data(0x03)         
-    epd_send_command(SET_RAM_X_ADDRESS_START_END_POSITION)  # Set RAM X - address Start / End position
-    epd_send_data(0x00)  #  RAM x address start at 0
-    epd_send_data(0x31)  # RAM x address end at 31h(49+1)*8->400
-    epd_send_command(SET_RAM_Y_ADDRESS_START_END_POSITION)  # Set Ram Y- address Start / End position
-    epd_send_data(0x2B)  #  RAM y address start at  12Bh  
-    epd_send_data(0x01)
-    epd_send_data(0x00)  #  RAM y address end at 00h
-    epd_send_data(0x00)      
-        
-    epd_send_command(BORDER_WAVEFORM_CONTROL)  #  Border Waveform Control
-    epd_send_data(0x01)  #  HIZ
+    def epd_set_cursor(self, x_start, y_start):
+        self.epd_send_command(SET_RAM_X_ADDRESS_COUNTER)
+        self.epd_send_data((x_start >> 3) & 0xFF)
 
-    epd_send_command(DISPLAY_UPDATE_CONTROL_1)
-    epd_send_data(0x80) # Inverse RED RAM content
+        self.epd_send_command(self, SET_RAM_Y_ADDRESS_COUNTER)
+        self.epd_send_data(y_start & 0xFF)
+        self.epd_send_data((y_start >> 8) & 0xFF)
 
+    def epd_clear(self):
+        #width = (EPD_WIDTH % 8 == 0)? (EPD_WIDTH / 8 ): (EPD_WIDTH / 8 + 1);
+        width = (EPD_WIDTH / 8 ) if (EPD_WIDTH % 8 == 0) else (EPD_WIDTH / 8 + 1)
+        width = int(width)
+        height = EPD_HEIGHT;
+        self.epd_set_windows(0, 0, EPD_WIDTH, EPD_HEIGHT)
+        # for loop black
+        for j in range(height):
+            self.epd_set_cursor(0, j)
+            self.epd_send_command(WRITE_RAM)
+            for i in range(width):
+                self.epd_send_data(0XFF)
+
+        # for loop red
+        for j in range(height):
+            self.epd_set_cursor(0, j)
+            self.epd_send_command(WRITE_RAM_RED)
+            for i in range(width):
+                self.epd_send_data(0XFF)
+        self.epd_turn_on_display()
+
+    def epd_sleep(self):
+        self.epd_send_command(DEEP_SLEEP_MODE)
+        self.epd_send_data(0x01)
+
+    def epd_reset(self):
+        self.digital_write(RST_PIN, 1)
+        delay_ms(200)
+        self.digital_write(RST_PIN, 0)
+        delay_ms(200)
+        self.digital_write(RST_PIN, 1)
+        delay_ms(200)
+
+    def epd_init(self):
+        self.epd_reset()
+
+        self.epd_send_command(0x74)
+        self.epd_send_data(0x54)
+        self.epd_send_command(0x7E)
+        self.epd_send_data(0x3B)
+        self.epd_send_command(0x2B)   #  Reduce glitch under ACVCOM  
+        self.epd_send_data(0x04)           
+        self.epd_send_data(0x63)
             
-    epd_send_command(0x18) # Temperature Sensor Control
-    epd_send_data(0x80)   # Internal temperature sensor
-    epd_send_command(DISPLAY_UPDATE_CONTROL_2) # Display UpdateControl 2
-    epd_send_data(0xB1)  # Load Temperature and waveform setting.
-    epd_send_command(MASTER_ACTIVATION)  # Master Activation
-    epd_wait_until_idle()
+        self.epd_send_command(0x0C)   #  Soft start setting
+        self.epd_send_data(0x8E)           
+        self.epd_send_data(0x8C)
+        self.epd_send_data(0x85)
+        self.epd_send_data(0x3F)    
 
-def epd_connect(bus=0, device=0)
-    # bus, device = 0, 0  # /dev/spidev<bus>.<device>
-    spi = spidev.SpiDev()
-    spi.open(bus, device)
 
-    # init GPIO
-    gpio.setmode(gpio.BCM)
-    gpio.setwarnings(False)
-    gpio.setup(RST_PIN, gpio.OUT)
-    gpio.setup(DC_PIN, gpio.OUT)
-    gpio.setup(CS_PIN, gpio.OUT)
-    gpio.setup(BUSY_PIN, gpio.IN)
+        self.epd_send_command(DRIVER_OUTPUT_CONTROL)   #  Driver Output control  Set MUX as 300
+        self.epd_send_data(0x2B)           
+        self.epd_send_data(0x01)
+        self.epd_send_data(0x00)  
+        self.epd_send_command(DATA_ENTRY_MODE_SETTING)   #  Data Entry mode setting
+        self.epd_send_data(0x03)         
+        self.epd_send_command(SET_RAM_X_ADDRESS_START_END_POSITION)  # Set RAM X - address Start / End position
+        self.epd_send_data(0x00)  #  RAM x address start at 0
+        self.epd_send_data(0x31)  # RAM x address end at 31h(49+1)*8->400
+        self.epd_send_command(SET_RAM_Y_ADDRESS_START_END_POSITION)  # Set Ram Y- address Start / End position
+        self.epd_send_data(0x2B)  #  RAM y address start at  12Bh  
+        self.epd_send_data(0x01)
+        self.epd_send_data(0x00)  #  RAM y address end at 00h
+        self.epd_send_data(0x00)      
+            
+        self.epd_send_command(BORDER_WAVEFORM_CONTROL)  #  Border Waveform Control
+        self.epd_send_data(0x01)  #  HIZ
 
-    spi.max_speed_hz = 32000000  # is this 32Mhz? units are not documented in wiringpim struct implies this is hz
-    spi.mode = 0b00
+        self.epd_send_command(DISPLAY_UPDATE_CONTROL_1)
+        self.epd_send_data(0x80) # Inverse RED RAM content
+
+                
+        self.epd_send_command(0x18) # Temperature Sensor Control
+        self.epd_send_data(0x80)   # Internal temperature sensor
+        self.epd_send_command(DISPLAY_UPDATE_CONTROL_2) # Display UpdateControl 2
+        self.epd_send_data(0xB1)  # Load Temperature and waveform setting.
+        self.epd_send_command(MASTER_ACTIVATION)  # Master Activation
+        self.epd_wait_until_idle()
+
+
 
 
 def main(argv=None):
@@ -220,14 +223,14 @@ def main(argv=None):
 
     #import pdb ; pdb.set_trace()
 
-    epd_connect()
+    epd = Epd()
 
-    epd_init()
-    epd_clear()
-    epd_sleep()
+    epd.epd_init()
+    epd.epd_clear()
+    epd.epd_sleep()
 
     # need to reset GPIO?
-    spi.close()
+    epd.spi.close()
 
     return 0
 
