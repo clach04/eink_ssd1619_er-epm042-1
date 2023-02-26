ab437ba8cb4b8ef1f0c5b55210306e786ab1d9c2
clach04 <clach04@gmail.com>
2023-02-26 10:36:00 -0800

    Old PIL workaround

--- ER-EPM042-1_Raspberry_Pi/ER-EPM042A1-1R/python/display_image.py
+++ ER-EPM042-1_Raspberry_Pi/ER-EPM042A1-1R/python/display_image.py
@@ -29,7 +29,49 @@ pil_version = tuple(map(int, PIL.__version__.split('.')))
 MAX_IMAGE_SIZE = (epd.EPD_WIDTH, epd.EPD_HEIGHT)
 
 
+def simpleimage_resize_orig(im):
+    log.debug('im.size %r', im.size)
+    if im.size[1] > im.size[0]:
+        log.debug('rotate')
+        #im = im.rotate(90)
+        #im = im.transpose(Image.ROTATE_90)
+        im = im.transpose(Image.ROTATE_270)
+    log.debug('im.size %r', im.size)
+    if im.size > MAX_IMAGE_SIZE:
+        log.debug('resizing')
+        """resize - maintain aspect ratio
+        NOTE PIL thumbnail method does not increase
+        if new size is larger than original
+        2 passes gives good speed and quality
+        """
+        im.thumbnail((MAX_IMAGE_SIZE[0] * 2, MAX_IMAGE_SIZE[1] * 2))
+        im.thumbnail(MAX_IMAGE_SIZE, Image.ANTIALIAS)
+
+    # image is not too big, but it may be too small
+    # _may_ need to add white (black) bar(s)
+    if im.size < MAX_IMAGE_SIZE:
+        log.debug('too small - add bar')
+        im = im.convert('RGB')  # convert to RGB
+        #bg_col = (0, 0, 0)
+        #bg_col = (0x00, 0x00, 0x00)
+        bg_col = (0xff, 0xff, 0xff)
+        background = Image.new('RGB', MAX_IMAGE_SIZE, bg_col)
+        x, y = im.size
+        #background.paste(im, (0, 0, x, y))  # does not center/centre
+        x1 = int(0.5 * (background.size[0] - im.size[0]))
+        y1 = int(.5 * (background.size[1] - im.size[1]))
+        x2 = int(.5 * (background.size[0] + im.size[0]))
+        y2 = int(.5 * (background.size[1] + im.size[1]))
+        print(im.size)
+        print(background.size)
+        print((x1, y1, x2, y2))
+        background.paste(im, (x1, y1, x2, y2))  # center/centre
+        im = background
+
+    return im
+
 def simpleimage_resize(im):
+    # works fine with pil_version (7, 1, 2), but with (5, 4, 1) images end up squashed
     log.debug('im.size %r', im.size)
     if im.size[1] > im.size[0]:
         log.debug('rotate')
@@ -40,6 +82,8 @@ def simpleimage_resize(im):
     im = ImageOps.pad(im, MAX_IMAGE_SIZE, method=Image.BICUBIC, color=(0xff, 0xff, 0xff), centering=(0.5, 0.5))
     return im
 
+if pil_version <= (5, 4, 1):
+    simpleimage_resize = simpleimage_resize_orig
 
 print('Python %s on %s' % (sys.version, sys.platform))
 
